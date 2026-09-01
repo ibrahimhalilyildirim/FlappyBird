0a94105cab32fe6c56dcefa7ea3022aefa99f965
Ibrahim Halil Yildirim <ibrahimhalilyildirim09@gmail.com>
2026-09-01 12:29:11 +0300

    v1.11

--- Assets/_GameAssets/Scripts/StartButtonScript.cs
+++ Assets/_GameAssets/Scripts/StartButtonScript.cs
@@ -6,8 +6,12 @@ using UnityEngine.UI;
 public class StartButtonScript : MonoBehaviour
 {
     [SerializeField] private Button _gameStartButton;
+    [SerializeField] private Button _musicButton;
+    [SerializeField] private Image _musicOnImage;
+    [SerializeField] private Image _musicOffImage;
     private void Awake()
     {
+        BackgroundMusic.Instance.RegisterMusicButton(_musicButton, _musicOnImage, _musicOffImage);
         _gameStartButton.onClick.AddListener(() => SceneManager.LoadScene(Consts.SceneNames.GAME_SCENE));
         
     }
